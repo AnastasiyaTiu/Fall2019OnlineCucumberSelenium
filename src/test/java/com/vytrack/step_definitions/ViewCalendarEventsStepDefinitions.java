58254977a71c1b4bf731c524186cc1c791e1a469
AnastasiyaTiu <stacyti8888@gmail.com>
2020-05-02 22:54:56 -0700

    feature

---
+++ src/test/java/com/vytrack/step_definitions/ViewCalendarEventsStepDefinitions.java
@@ -0,0 +1,18 @@
+package com.vytrack.step_definitions;
+
+import com.vytrack.pages.activities.CalendarsEventsPage;
+import io.cucumber.java.en.Then;
+
+import java.util.List;
+
+public class ViewCalendarEventsStepDefinitions {
+
+    CalendarsEventsPage calendarsEventsPage = new CalendarsEventsPage();
+
+    @Then("View Per Page menu should have following options")
+    public void view_Per_Page_menu_should_have_following_options(List<String> dataTable) {
+
+    }
+
+
+}
