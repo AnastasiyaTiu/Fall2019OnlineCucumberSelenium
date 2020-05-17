40337e9328c7667614da566d06100283f85d81f4
AnastasiyaTiu <stacyti8888@gmail.com>
2020-05-17 16:36:56 -0700

    regression

--- src/test/java/com/vytrack/runners/RegressionRunner.java
+++ src/test/java/com/vytrack/runners/RegressionRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/default-report-for-regression",
                 "json:target/cucumber_regression.json",
-                "return:target/return.txt"
+                "return:target/return_regression.txt"
         }
 )
 
